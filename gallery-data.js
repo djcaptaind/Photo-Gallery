e5fec7e2ff0de85e202daacd031229c4fa026f36
djcaptaind <djcaptaind@gmail.com>
2026-08-26 18:17:42 -0500

    Update gallery-data.js

--- gallery-data.js
+++ gallery-data.js
@@ -1,16 +1,1 @@
-window.CALLAWAY_GALLERY = [
-  {
-    "image": "images/gala.jpg",
-    "title": "gala",
-    "caption": "",
-    "category": "leadership",
-    "layout": "normal"
-  },
-  {
-    "image": "images/gala.jpg",
-    "title": "gala",
-    "caption": "",
-    "category": "events",
-    "layout": "normal"
-  }
-];
+window.CALLAWAY_GALLERY = [];
